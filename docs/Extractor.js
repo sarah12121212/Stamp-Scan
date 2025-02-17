554991cb9c56c117dddc81366f0218701042747a
sarah12121212 <sarah.michhelle@gmail.com>
2025-02-16 22:44:41 -0700

    commit

---
+++ docs/Extractor.js
@@ -0,0 +1,19 @@
+
+
+const extractor = ml5.featureExtractor('MobileNet', modelLoaded);
+
+let images = []
+
+function fetchJSON(){
+    fetch('./images.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error! Status: ${response.status}`);
+        }
+        return response.json();  
+    })
+    .then(data => console.log(data))  
+    .catch(error => console.error('Failed to fetch data:', error)); 
+    
+}
+
